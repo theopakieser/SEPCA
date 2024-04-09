e832e3099df94a6d9ddb61a979d7dfa4a65e47cb
Rampant098 <122899668+Rampant098@users.noreply.github.com>
2024-04-09 10:53:31 +0100

    appointments class

---
+++ src/main/java/Appointments/Appointments.java
@@ -0,0 +1,73 @@
+package Appointments;
+import java.util.ArrayList;
+
+public class Appointments {
+    public static ArrayList<Appointments> appointments= new ArrayList<>();
+
+    private String date, appointmentType,  time;
+
+    public Appointments(String  date, String  time, String appointmentType) {
+        this.date = date;
+        this.time = time;
+        this.appointmentType = appointmentType;
+
+    }
+
+    public String  getDate() {
+        return date;
+    }
+
+    public void setDate(String  date) {
+        this.date = date;
+    }
+
+    public String  getTime() {
+        return time;
+    }
+
+    public void setTime(String  time) {
+        this.time = time;
+    }
+
+    public String getAppointmentType() {
+        return appointmentType;
+    }
+
+    public void setAppointmentType(String appointmentType) {
+        if(appointmentType.equalsIgnoreCase("library") || appointmentType.equalsIgnoreCase("counselling") || appointmentType.equals("maths and learning centre"));
+        this.appointmentType = appointmentType;
+    }
+
+    public static void add(Appointments appointment){
+        appointments.add(appointment);
+    }
+
+    public static void delete(Appointments appointment){
+        appointments.remove(appointment);
+    }
+
+    public String listApps(){
+        StringBuilder list = new StringBuilder();
+        for(Appointments appointments:appointments){
+            list.append(appointments.getAppointmentType()).append(" ").append(appointments.getDate()).append("\n");
+        }
+        return list.toString();
+    }
+
+
+    public int amount(){
+        return appointments.size();
+    }
+
+
+    @Override
+    public String toString() {
+        return "Appointments{" +
+                ", date=" + date +
+                ", time=" + time +
+                ", appointmentType='" + appointmentType + '\'' +
+                '}';
+    }
+
+
+}
